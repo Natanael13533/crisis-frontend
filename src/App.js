eeb6bcf4c8879168e754e12271beeae7013d04e7
Natanael13533 <jamesnatan89@gmail.com>
2024-07-17 09:15:11 +0700

    deploy react

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/"
+            path="/crisis-project"
             element = {
               <Navbar title="Dashboard">
                 <Dashboard/>
